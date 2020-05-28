c72608eb8d6603400d6104f0d97818afa8f560fb
vietnguyen <vietnguyen.nqv@gmail.com>
2020-05-28 19:53:22 +1000

    Updates

---
+++ precache-manifest.0a0f3e12da658d1eab42746f1a8a2e22.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e8738c6545b299a146a54e05228555",
+    "url": "/vietnguyen88/sneaker-store.git/index.html"
+  },
+  {
+    "revision": "7ea19e6b72f96cf233f8",
+    "url": "/vietnguyen88/sneaker-store.git/static/css/main.14075fe8.chunk.css"
+  },
+  {
+    "revision": "c0818ff74d65841dc448",
+    "url": "/vietnguyen88/sneaker-store.git/static/js/2.20005d4a.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/vietnguyen88/sneaker-store.git/static/js/2.20005d4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ea19e6b72f96cf233f8",
+    "url": "/vietnguyen88/sneaker-store.git/static/js/main.a2282a28.chunk.js"
+  },
+  {
+    "revision": "c634708d3ad38dd70a67",
+    "url": "/vietnguyen88/sneaker-store.git/static/js/runtime-main.0aec92f9.js"
+  },
+  {
+    "revision": "51690114351189df613b0785fa1a6d8c",
+    "url": "/vietnguyen88/sneaker-store.git/static/media/andres-jasso-u4unYsXQHnE-unsplash.51690114.jpg"
+  },
+  {
+    "revision": "bbf06e5fb78be44e6551858c832fcfa5",
+    "url": "/vietnguyen88/sneaker-store.git/static/media/imani-clovis-LxVxPA1LOVM-unsplash.bbf06e5f.jpg"
+  },
+  {
+    "revision": "c2a1f1503a204853934cb8df3704d44d",
+    "url": "/vietnguyen88/sneaker-store.git/static/media/leon-skibitzki-NMyuo1hoCAA-unsplash.c2a1f150.jpg"
+  },
+  {
+    "revision": "8b31a3cf42ddf2d418c1e0ee7c80a17a",
+    "url": "/vietnguyen88/sneaker-store.git/static/media/trendest-studio-XZ3EmAIWuz0-unsplash.8b31a3cf.jpg"
+  }
+]);
